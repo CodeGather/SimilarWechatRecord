5bf1729fd0e897a3d4b1d3e341a5e72ec2a792a2
21克的爱情 <raohong07@163.com>
2018-12-10 14:01:04 +0800

    修改

--- WeiXinRecord/SimilarWechatRecord/src/main/java/com/raohong/SimilarWechatRecord/EditVideoActivity.java
+++ WeiXinRecord/SimilarWechatRecord/src/main/java/com/raohong/SimilarWechatRecord/EditVideoActivity.java
@@ -1,5 +1,7 @@
 package com.raohong.SimilarWechatRecord;
 
+
+import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.ValueAnimator;
 import android.annotation.SuppressLint;
@@ -10,7 +12,6 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.drawable.BitmapDrawable;
-import android.media.MediaMetadataRetriever;
 import android.media.MediaPlayer;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -27,6 +28,7 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -37,10 +39,10 @@ import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.Locale;
 
 /**
- * Created by raohong on 17/2/21.
+ * Created by zhaoshuang on 17/2/21.
  * 视频编辑界面
  */
 
@@ -52,15 +54,14 @@ public class EditVideoActivity extends BaseActivity {
     private int[] drawableBg = new int[]{R.drawable.color1, R.drawable.color2, R.drawable.color3, R.drawable.color4, R.drawable.color5};
     private int[] colors = new int[]{R.color.color1, R.color.color2, R.color.color3, R.color.color4, R.color.color5};
     private int[] expressions = new int[]{R.mipmap.logo};
-    private int dp100;
-    private int dp90;
-    private int dp30;
-    private int dp25;
     private int dp20;
-    private int dp10;
+    private int dp25;
 
     private int currentColorPosition;
     private TuyaView tv_video;
+    private ImageView iv_pen;
+    private ImageView iv_icon;
+    private RelativeLayout rl_expression;
     private RelativeLayout rl_touch_view;
     private RelativeLayout rl_edit_text;
     private EditText et_tag;
@@ -74,6 +75,11 @@ public class EditVideoActivity extends BaseActivity {
     private RelativeLayout rl_title;
     private RelativeLayout rl_bottom;
     private TextView tv_hint_delete;
+    private int dp100;
+    private ImageView iv_speed;
+    private LinearLayout ll_progress;
+    private SeekBar sb_speed;
+    private TextView tv_speed;
     private float videoSpeed = 1;
     private int videoWidth;
     private int videoHeight;
@@ -89,9 +95,6 @@ public class EditVideoActivity extends BaseActivity {
         windowHeight = getWindowManager().getDefaultDisplay().getHeight();
 
         dp100 = (int) getResources().getDimension(R.dimen.dp100);
-        dp90 = (int) getResources().getDimension(R.dimen.dp90);
-        dp30 = (int) getResources().getDimension(R.dimen.dp30);
-        dp10 = (int) getResources().getDimension(R.dimen.dp10);
 
         initUI();
         initData();
@@ -134,13 +137,30 @@ public class EditVideoActivity extends BaseActivity {
                 }
             }
         });
+
+        //当进行涂鸦操作时, 隐藏标题栏和底部工具栏
+        tv_video.setOnTouchListener(new TuyaView.OnTouchListener() {
+            @Override
+            public void onDown() {
+                changeMode(false);
+            }
+
+            @Override
+            public void onUp() {
+                changeMode(true);
+            }
+        });
     }
 
     private void initUI() {
 
         vv_play = (MyVideoView) findViewById(R.id.vv_play);
+        RelativeLayout rl_pen = (RelativeLayout) findViewById(R.id.rl_pen);
+        RelativeLayout rl_icon = (RelativeLayout) findViewById(R.id.rl_icon);
+        RelativeLayout rl_text = (RelativeLayout) findViewById(R.id.rl_text);
         ll_color = (LinearLayout) findViewById(R.id.ll_color);
         tv_video = (TuyaView) findViewById(R.id.tv_video);
+        rl_expression = (RelativeLayout) findViewById(R.id.rl_expression);
         rl_touch_view = (RelativeLayout) findViewById(R.id.rl_touch_view);
         TextView tv_close = (TextView) findViewById(R.id.tv_close);
         TextView tv_finish = (TextView) findViewById(R.id.tv_finish);
@@ -148,15 +168,52 @@ public class EditVideoActivity extends BaseActivity {
         et_tag = (EditText) findViewById(R.id.et_tag);
         tv_tag = (TextView) findViewById(R.id.tv_tag);
         TextView tv_finish_video = (TextView) findViewById(R.id.tv_finish_video);
+        iv_pen = (ImageView) findViewById(R.id.iv_pen);
+        iv_icon = (ImageView) findViewById(R.id.iv_icon);
+        iv_speed = (ImageView) findViewById(R.id.iv_speed);
         rl_tuya = (RelativeLayout) findViewById(R.id.rl_tuya);
         rl_close = (RelativeLayout) findViewById(R.id.rl_close);
         rl_title = (RelativeLayout) findViewById(R.id.rl_title);
         rl_bottom = (RelativeLayout) findViewById(R.id.rl_bottom);
         tv_hint_delete = (TextView) findViewById(R.id.tv_hint_delete);
+        RelativeLayout rl_speed = (RelativeLayout) findViewById(R.id.rl_speed);
+        ll_progress = (LinearLayout) findViewById(R.id.ll_progress);
+        sb_speed = (SeekBar) findViewById(R.id.sb_speed);
+        tv_speed = (TextView) findViewById(R.id.tv_speed);
         RelativeLayout rl_cut_size = (RelativeLayout) findViewById(R.id.rl_cut_size);
         RelativeLayout rl_cut_time = (RelativeLayout) findViewById(R.id.rl_cut_time);
 
         RelativeLayout rl_back = (RelativeLayout) findViewById(R.id.rl_back);
+
+        rl_pen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changePenState(!(ll_color.getVisibility() == View.VISIBLE));
+                changeIconState(false);
+                changeTextState(false);
+                changeSpeedState(false);
+            }
+        });
+
+        rl_icon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeIconState(!(rl_expression.getVisibility() == View.VISIBLE));
+                changePenState(false);
+                changeTextState(false);
+                changeSpeedState(false);
+            }
+        });
+
+        rl_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeTextState(!(rl_edit_text.getVisibility() == View.VISIBLE));
+                changePenState(false);
+                changeIconState(false);
+                changeSpeedState(false);
+            }
+        });
 
         rl_back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -165,10 +222,17 @@ public class EditVideoActivity extends BaseActivity {
             }
         });
 
+        tv_close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeTextState(!(rl_edit_text.getVisibility() == View.VISIBLE));
+            }
+        });
 
         tv_finish.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                changeTextState(!(rl_edit_text.getVisibility() == View.VISIBLE));
                 if (et_tag.getText().length() > 0) {
                     addTextToWindow();
                 }
@@ -186,6 +250,16 @@ public class EditVideoActivity extends BaseActivity {
             @Override
             public void onClick(View v) {
                 onBackPressed();
+            }
+        });
+
+        rl_speed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeTextState(false);
+                changePenState(false);
+                changeIconState(false);
+                changeSpeedState(!(ll_progress.getVisibility() == View.VISIBLE));
             }
         });
 
@@ -207,6 +281,7 @@ public class EditVideoActivity extends BaseActivity {
 
         initColors();
         initExpression();
+        initSpeed();
 
         et_tag.addTextChangedListener(new TextWatcher() {
             @Override
@@ -222,6 +297,31 @@ public class EditVideoActivity extends BaseActivity {
             @Override
             public void afterTextChanged(Editable s) {
                 tv_tag.setText(s.toString());
+            }
+        });
+    }
+
+    private void initSpeed() {
+
+        sb_speed.setMax(200);
+        sb_speed.setProgress(100);
+        sb_speed.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (progress < 50) {
+                    progress = 50;
+                    sb_speed.setProgress(50);
+                }
+                videoSpeed = progress / 100f;
+                tv_speed.setText(videoSpeed + "");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
             }
         });
     }
@@ -242,10 +342,26 @@ public class EditVideoActivity extends BaseActivity {
      */
     private void initExpression() {
 
-        ImageView imageView = new ImageView(this);
-        imageView.setPadding(dp10, dp10, dp10, dp10);
-        final int result = expressions[0];
-        addExpressionToWindow(result);
+        int dp80 = (int) getResources().getDimension(R.dimen.dp80);
+        int dp10 = (int) getResources().getDimension(R.dimen.dp10);
+        for (int x = 0; x < expressions.length; x++) {
+            ImageView imageView = new ImageView(this);
+            imageView.setPadding(dp10, dp10, dp10, dp10);
+            final int result = expressions[x];
+            imageView.setImageResource(result);
+            imageView.setLayoutParams(new ViewGroup.LayoutParams(windowWidth / 4, dp80));
+            imageView.setX(x % 4 * windowWidth / 4);
+            imageView.setY(x / 4 * dp80);
+            imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    rl_expression.setVisibility(View.GONE);
+                    iv_icon.setImageResource(R.mipmap.icon);
+                    addExpressionToWindow(result);
+                }
+            });
+            rl_expression.addView(imageView);
+        }
     }
 
     /**
@@ -255,12 +371,44 @@ public class EditVideoActivity extends BaseActivity {
 
         TouchView touchView = new TouchView(this);
         touchView.setBackgroundResource(result);
-        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(dp90, dp30);
-        /* 上下左右居中 */
-        //layoutParams.addRule(RelativeLayout.CENTER_IN_PARENT);
-        layoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
-        layoutParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(dp100, dp100);
+        layoutParams.addRule(RelativeLayout.CENTER_IN_PARENT);
         touchView.setLayoutParams(layoutParams);
+
+        touchView.setLimitsX(0, windowWidth);
+        touchView.setLimitsY(0, windowHeight - dp100 / 2);
+        touchView.setOnLimitsListener(new TouchView.OnLimitsListener() {
+            @Override
+            public void OnOutLimits(float x, float y) {
+                tv_hint_delete.setTextColor(Color.RED);
+            }
+
+            @Override
+            public void OnInnerLimits(float x, float y) {
+                tv_hint_delete.setTextColor(Color.WHITE);
+            }
+        });
+        touchView.setOnTouchListener(new TouchView.OnTouchListener() {
+            @Override
+            public void onDown(TouchView view, MotionEvent event) {
+                tv_hint_delete.setVisibility(View.VISIBLE);
+                changeMode(false);
+            }
+
+            @Override
+            public void onMove(TouchView view, MotionEvent event) {
+
+            }
+
+            @Override
+            public void onUp(TouchView view, MotionEvent event) {
+                tv_hint_delete.setVisibility(View.GONE);
+                changeMode(true);
+                if (view.isOutLimits()) {
+                    rl_touch_view.removeView(view);
+                }
+            }
+        });
 
         rl_touch_view.addView(touchView);
     }
@@ -409,6 +557,58 @@ public class EditVideoActivity extends BaseActivity {
     }
 
     /**
+     * 更改画笔状态的界面
+     */
+    private void changePenState(boolean flag) {
+
+        if (flag) {
+            tv_video.setDrawMode(flag);
+            tv_video.setNewPaintColor(getResources().getColor(colors[currentColorPosition]));
+            iv_pen.setImageResource(R.mipmap.pen_click);
+            ll_color.setVisibility(View.VISIBLE);
+        } else {
+            tv_video.setDrawMode(flag);
+            ll_color.setVisibility(View.GONE);
+            iv_pen.setImageResource(R.mipmap.pen);
+        }
+    }
+
+    /**
+     * 更改表情状态的界面
+     */
+    private void changeIconState(boolean flag) {
+
+        if (flag) {
+            iv_icon.setImageResource(R.mipmap.icon_click);
+            rl_expression.setVisibility(View.VISIBLE);
+        } else {
+            rl_expression.setVisibility(View.GONE);
+            iv_icon.setImageResource(R.mipmap.icon);
+        }
+    }
+
+    /**
+     * 更改文字输入状态的界面
+     */
+    private void changeTextState(boolean flag) {
+
+        if (flag) {
+            rl_edit_text.setY(windowHeight);
+            rl_edit_text.setVisibility(View.VISIBLE);
+            startAnim(rl_edit_text.getY(), 0, null);
+            popupEditText();
+        } else {
+            manager.hideSoftInputFromWindow(et_tag.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+            startAnim(0, windowHeight, new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    rl_edit_text.setVisibility(View.GONE);
+                }
+            });
+        }
+    }
+
+    /**
      * 更改视频剪切界面
      */
     private void changeCutState() {
@@ -416,6 +616,20 @@ public class EditVideoActivity extends BaseActivity {
         Intent intent = new Intent(this, CutSizeActivity.class);
         intent.putExtra("path", path);
         startActivityForResult(intent, 1);
+    }
+
+    /**
+     * 更改视频加速
+     */
+    private void changeSpeedState(boolean flag) {
+
+        if (flag) {
+            ll_progress.setVisibility(View.VISIBLE);
+            iv_speed.setImageResource(R.mipmap.speed_click);
+        } else {
+            ll_progress.setVisibility(View.GONE);
+            iv_speed.setImageResource(R.mipmap.speed);
+        }
     }
 
     /**
@@ -444,7 +658,6 @@ public class EditVideoActivity extends BaseActivity {
 
         String mergeVideo = SDKUtil.VIDEO_PATH + "/mergeVideo.mp4";
 
-
         //ffmpeg -i videoPath -i imagePath -filter_complex overlay=0:0 -vcodec libx264 -profile:v baseline -preset ultrafast -b:v 3000k -g 30 -f mp4 outPath
         StringBuilder sb = new StringBuilder();
         sb.append("ffmpeg");
@@ -466,44 +679,40 @@ public class EditVideoActivity extends BaseActivity {
         }
     }
 
-
     /**
-     * 获取本地或者url视频的第一帧
-     * @param fileData
-     * @return
+     * 调整视频播放速度
      */
-    public static Bitmap getVideoThumbnail(String fileData) {
-        Bitmap bitmap = null;
-        /*
-         * MediaMetadataRetriever 是android中定义好的一个类，提供了统一的接口，
-         * 用于从输入的媒体文件中取得帧和元数据
-         */
-        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
-        try {
-            if( fileData.contains("http://") || fileData.contains("https://") ) {
-                System.out.println("包含该字符串");
-                //根据文件路径获取缩略图
-                retriever.setDataSource(fileData, new HashMap());
-            } else {
-                //根据文件路径获取缩略图
-                retriever.setDataSource(fileData);
-            }
-            //获得第一帧图片
-            bitmap = retriever.getFrameAtTime();
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-        } finally {
-            retriever.release();
-        }
+    private String adjustVideoSpeed(String path, float speed) {
 
-        System.out.println("保存成功");
-        return bitmap;
+        String outPut = SDKUtil.VIDEO_PATH + "/speedVideo.mp4";
+
+        //./ffmpeg -i 2x.mp4 -filter_complex "[0:v]setpts=0.5*PTS[v];[0:a]atempo=2.0[a]" -map "[v]" -map "[a]" output3.mp4
+        String filter = String.format(Locale.getDefault(), "[0:v]setpts=%f*PTS[v];[0:a]atempo=%f[a]", 1 / speed, speed);
+        StringBuilder sb = new StringBuilder("ffmpeg");
+        sb.append(" -i");
+        sb.append(" " + path);
+        sb.append(" -filter_complex");
+        sb.append(" " + filter);
+        sb.append(" -map");
+        sb.append(" [v]");
+        sb.append(" -map");
+        sb.append(" [a]");
+        sb.append(" -y");
+        sb.append(" " + outPut);
+
+        int i = UtilityAdapter.FFmpegRun("", sb.toString());
+        if (i == 0) {
+            return outPut;
+        } else {
+            return "";
+        }
     }
 
     @Override
     public void onBackPressed() {
         setResult(RESULT_OK);
         if(rl_edit_text.getVisibility() == View.VISIBLE){
+            changeTextState(false);
         }else {
             super.onBackPressed();
         }
@@ -542,6 +751,7 @@ public class EditVideoActivity extends BaseActivity {
 
     private boolean isPen;
     private boolean isImage;
+    private boolean isSpeed;
 
     @SuppressLint("StaticFieldLeak")
     private void finishVideo() {
@@ -558,7 +768,13 @@ public class EditVideoActivity extends BaseActivity {
             isImage = true;
         }
 
-        if(!isPen && !isImage){
+        if(sb_speed.getProgress() == 100){
+            isSpeed = false;
+        }else{
+            isSpeed = true;
+        }
+
+        if(!isPen && !isImage && !isSpeed){
             Intent intent = new Intent();
             intent.putExtra("videoPath", path);
             setResult(RESULT_OK, intent);
@@ -567,7 +783,6 @@ public class EditVideoActivity extends BaseActivity {
         }
 
         new AsyncTask<Void, Void, String>() {
-
             @Override
             protected void onPreExecute() {
                 showProgressDialog();
@@ -576,6 +791,9 @@ public class EditVideoActivity extends BaseActivity {
             @Override
             protected String doInBackground(Void... params) {
                 String videoPath = mergeImage(path);
+                if (videoSpeed != 1) {
+                    videoPath = adjustVideoSpeed(videoPath, videoSpeed);
+                }
                 return videoPath;
             }
 
@@ -586,27 +804,6 @@ public class EditVideoActivity extends BaseActivity {
                     Intent intent = new Intent();
                     intent.putExtra("videoPath", result);
                     setResult(RESULT_OK, intent);
-
-
-                    String mergeVideo = SDKUtil.VIDEO_PATH + "/mergeVideo.mp4";
-
-                    String imagePaths = SDKUtil.VIDEO_PATH + "/ThumIMG.png";
-
-                    Bitmap bitmaps = getVideoThumbnail(mergeVideo);
-
-                    System.out.println("已保存视频"+mergeVideo);
-
-                    File files = new File(imagePaths);//将要保存图片的路径
-                    try {
-                        BufferedOutputStream boss = new BufferedOutputStream(new FileOutputStream(files));
-                        bitmaps.compress(Bitmap.CompressFormat.PNG, 100, boss);
-                        boss.flush();
-                        boss.close();
-                        System.out.println("已保存缩略图"+imagePaths);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-
                     finish();
                 } else {
                     Toast.makeText(getApplicationContext(), "视频编辑失败", Toast.LENGTH_SHORT).show();
