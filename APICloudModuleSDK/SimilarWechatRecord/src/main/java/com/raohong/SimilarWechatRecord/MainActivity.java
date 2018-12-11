f1d41c055d0b1c71a51206791023d1d44edd72a2
21克的爱情 <raohong07@163.com>
2018-12-11 13:58:01 +0800

    视频录制修改完成

--- APICloudModuleSDK/SimilarWechatRecord/src/main/java/com/raohong/SimilarWechatRecord/MainActivity.java
+++ APICloudModuleSDK/SimilarWechatRecord/src/main/java/com/raohong/SimilarWechatRecord/MainActivity.java
@@ -1,4 +1,25 @@
 package com.raohong.SimilarWechatRecord;
 
-public class MainActivity {
+import android.content.Intent;
+
+import com.uzmap.pkg.uzcore.UZWebView;
+import com.uzmap.pkg.uzcore.uzmodule.UZModule;
+import com.uzmap.pkg.uzcore.uzmodule.UZModuleContext;
+
+public class MainActivity extends UZModule {
+
+    static final int ACTIVITY_REQUEST_CODE_A = 100;
+    private UZModuleContext mJsCallback;
+
+    public MainActivity(UZWebView webView) {
+        super(webView);
+    }
+
+    public void jsmethod_startRecordVideo(final UZModuleContext moduleContext){
+        mJsCallback = moduleContext;
+        Intent intent = new Intent(context(), RecordedActivity.class);
+        intent.putExtra("appParam", moduleContext.optString("maxTime"));
+        intent.putExtra("needResult", true);
+        startActivityForResult(intent, ACTIVITY_REQUEST_CODE_A);
+    }
 }
