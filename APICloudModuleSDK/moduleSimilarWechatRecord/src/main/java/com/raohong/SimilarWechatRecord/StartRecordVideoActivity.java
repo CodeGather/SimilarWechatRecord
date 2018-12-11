d9c8a30a3b24c2b4ff9800da4267878da77294dc
21克的爱情 <raohong07@163.com>
2018-12-11 16:58:47 +0800

    提交修改

---
+++ APICloudModuleSDK/moduleSimilarWechatRecord/src/main/java/com/raohong/SimilarWechatRecord/StartRecordVideoActivity.java
@@ -0,0 +1,34 @@
+package com.raohong.SimilarWechatRecord;
+
+import android.content.Intent;
+import com.uzmap.pkg.uzcore.UZWebView;
+import com.uzmap.pkg.uzcore.uzmodule.UZModule;
+import com.uzmap.pkg.uzcore.uzmodule.UZModuleContext;
+
+/*
+ * 声明一个主类用于module.json中使用
+ * 以及在APICloudModuleSDK/app/build.gradle中加载模块
+ * 至此引用结束
+ * Created by raohong on 18/12/11.
+ */
+public class StartRecordVideoActivity extends UZModule {
+
+    static final int ACTIVITY_REQUEST_CODE_A = 100;
+    private UZModuleContext mJsCallback;
+
+    public StartRecordVideoActivity(UZWebView webView) {
+        super(webView);
+    }
+
+    /*
+     * jsmethod_startRecordVideo
+     * startRecordVideo为外部JS调用
+     */
+    public void jsmethod_startRecordVideo(final UZModuleContext moduleContext){
+        mJsCallback = moduleContext;
+        Intent intent = new Intent(context(), RecordedActivity.class);
+        intent.putExtra("appParam", moduleContext.optString("maxTime"));
+        intent.putExtra("needResult", true);
+        startActivityForResult(intent, ACTIVITY_REQUEST_CODE_A);
+    }
+}
